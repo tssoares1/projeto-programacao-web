65b5f2613b1c2f3d2aa34c0d5e3e1cbf5930383a
Tiago Soares <tiago.soares@nortrez.com>
2025-08-15 20:16:12 -0300

    first project with spring

---
+++ TIAGO_SOARES/initial-project-sping/src/main/java/com/initial_project_sping/InitialProjectSpingApplication.java
@@ -0,0 +1,13 @@
+package com.initial_project_sping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialProjectSpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialProjectSpingApplication.class, args);
+	}
+
+}
---
+++ TIAGO_SOARES/initial-project-sping/src/test/java/com/initial_project_sping/InitialProjectSpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initial_project_sping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialProjectSpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
